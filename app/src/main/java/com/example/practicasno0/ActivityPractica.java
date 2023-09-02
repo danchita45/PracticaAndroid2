8078eb46a4d4dcd3a9c275503f5d906907fe75e5
danchita45 <danchita45@gmail.com>
2023-09-02 10:07:37 -0600

    ya pasa los datos

--- app/src/main/java/com/example/practicasno0/ActivityPractica.java
+++ app/src/main/java/com/example/practicasno0/ActivityPractica.java
@@ -2,13 +2,45 @@ package com.example.practicasno0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class ActivityPractica extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practica);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String nombre = intent.getStringExtra("nombre");
+            String telefono = intent.getStringExtra("telefono");
+            String cumple = intent.getStringExtra("cumple");
+            String genero = intent.getStringExtra("genero");
+            String colores = intent.getStringExtra("colores");
+
+            // Muestra los datos en TextViews u otras vistas en este Activity
+            TextView nombreTextView = findViewById(R.id.textViewNombre);
+            TextView telefonoTextView = findViewById(R.id.textViewTelefono);
+            TextView cumpleTextView = findViewById(R.id.textViewCumple);
+            TextView generoTextView = findViewById(R.id.textViewGenero);
+            TextView coloresTextView = findViewById(R.id.textViewColores);
+
+            nombreTextView.setText("Nombre: " + nombre);
+            telefonoTextView.setText("Teléfono: " + telefono);
+            cumpleTextView.setText("Fecha de Cumpleaños: " + cumple);
+            generoTextView.setText("Género: " + genero);
+            coloresTextView.setText("Colores Preferidos: " + colores);
+        }
     }
+
+
 }
--- app/src/main/java/com/example/practicasno0/Formulario.java
+++ app/src/main/java/com/example/practicasno0/Formulario.java
@@ -2,15 +2,25 @@ package com.example.practicasno0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Formulario extends AppCompatActivity {
 
 
-    EditText Nombre,Password;
-    Button Cerrar,Accesar;
+    private EditText nombreEditText;
+    private EditText telefonoEditText;
+    private EditText cumpleEditText;
+    private RadioGroup generoRadioGroup;
+    private CheckBox[] coloresCheckBoxes;
+    Button camara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,65 @@ public class Formulario extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
 
 
+        camara  = findViewById(R.id.btnCamara);
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a  = new Intent(this, Camara.class);
+                startActivity(a);
 
+            }
+        });
+        nombreEditText = findViewById(R.id.editTextNombre);
+        telefonoEditText = findViewById(R.id.editTextTelefono);
+        cumpleEditText = findViewById(R.id.editTextCumple);
+        generoRadioGroup = findViewById(R.id.radioGroupGenero);
+        coloresCheckBoxes = new CheckBox[]{
+                findViewById(R.id.checkBoxRojo),
+                findViewById(R.id.checkBoxVerde),
+                findViewById(R.id.checkBoxAmarillo),
+                findViewById(R.id.checkBoxMorado),
+                findViewById(R.id.checkBoxAzul)
+        };
+        Button btnMostrar = findViewById(R.id.btnFormMostrar);
+        btnMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarInformacion();
+            }
+        });
+
+
+    }
+    private void mostrarInformacion() {
+        // Captura los valores ingresados por el usuario
+        String nombre = nombreEditText.getText().toString();
+        String telefono = telefonoEditText.getText().toString();
+        String cumple = cumpleEditText.getText().toString();
+
+        RadioButton radioButton = findViewById(generoRadioGroup.getCheckedRadioButtonId());
+        String genero = radioButton.getText().toString();
+
+        StringBuilder colores = new StringBuilder();
+        for (CheckBox checkBox : coloresCheckBoxes) {
+            if (checkBox.isChecked()) {
+                colores.append(checkBox.getText()).append(", ");
+            }
+        }
+        // Elimina la última coma y espacio si hay colores seleccionados
+        if (colores.length() > 0) {
+            colores.delete(colores.length() - 2, colores.length());
+        }
+
+        // Crea un Intent para pasar datos al segundo Activity
+        Intent intent = new Intent(this, ActivityPractica.class);
+        intent.putExtra("nombre", nombre);
+        intent.putExtra("telefono", telefono);
+        intent.putExtra("cumple", cumple);
+        intent.putExtra("genero", genero);
+        intent.putExtra("colores", colores.toString());
+
+        // Inicia el segundo Activity
+        startActivity(intent);
     }
 }
